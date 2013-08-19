dd6afc55629c764de87f5127f6499cadc080ab77
Michael Bardsley <michael@mic-b.co.uk>
2013-08-19 20:13:41 +0100

    Added cache and memcached tests file

---
+++ tests/CacheTest.php
@@ -0,0 +1,55 @@
+<?php
+
+use Orno\Cache\Cache;
+
+class cacheTest extends \PHPUnit_Framework_Testcase
+{
+    protected $cache;
+
+    protected $defaultArray = [
+        [
+            'id' => 1,
+            'name' => 'Michael',
+        ],[
+            'id' => 2,
+            'name' => 'David',
+        ],
+    ];
+
+    public function setup()
+    {
+        $adapter = $this->getMockBuilder('Orno\Cache\Adapter\Memcached')
+                                    ->disableOriginalConstructor()
+                                    ->getMock();
+
+        $adapter->expects($this->any())
+                      ->method('get')
+                      ->with($this->equalTo('user'))
+                      ->will($this->returnValue($this->defaultArray));
+
+        $adapter->expects($this->any())
+                      ->method('set')
+                      ->will($this->returnSelf());
+
+        $adapter->expects($this->any())
+                      ->method('delete')
+                      ->will($this->returnSelf());
+
+        $this->cache = new Cache($adapter);
+    }
+
+    public function testGet()
+    {
+        $this->assertSame($this->cache->get('user'), $this->defaultArray);
+    }
+
+    public function testSet()
+    {
+        $this->assertInstanceOf('Orno\Cache\Adapter\Memcached', $this->cache->set('key', 'value'));
+    }
+
+    public function testDelete()
+    {
+        $this->assertInstanceOf('Orno\Cache\Adapter\Memcached', $this->cache->delete('key'));
+    }
+}
---
+++ tests/adapters/MemcachedTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use Orno\Cache\Adapter\Memcached;
+
+class MemcachedTest extends \PHPUnit_Framework_Testcase
+{
+    protected $memcache;
+
+    public function setup()
+    {
+        
+    }
+}
