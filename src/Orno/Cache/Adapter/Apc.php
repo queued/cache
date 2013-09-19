8f90937bf9a0a69c3787f361185dbb956db2bb4b
Michael Bardsley <michael.bardsley@symphony-group.co.uk>
2013-09-19 14:36:57 +0100

    Added APC cache adapter

---
+++ src/Orno/Cache/Adapter/Apc.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * The Orno Component Library
+ *
+ * @author  Phil Bennett @philipobenito
+ * @license MIT (see LICENSE file)
+ */
+namespace Orno\Cache;
+
+use Orno\Cache\Exception\AdapaterNotAvailableException;
+
+/**
+ * Apc
+ *
+ * @author Michael Bardsley <me@mic-b.co.uk>
+ */
+class Apc extends AbstractAdapter
+{
+    /**
+     * Constructor
+     *
+     * @throws AdapaterNotAvailableException
+     */
+    public function __construct()
+    {
+        if (! extension_loaded('apc')) {
+            throw new AdapaterNotAvailableException('The APC extension is not loaded');
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return void
+     */
+    public function get($key)
+    {
+        return apc_fetch($key);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return \Orno\Cache\Adapter\Apc
+     */
+    public function set($key, $data, $expiry = null)
+    {
+        if (is_null($expiry)) {
+            $expiry = $this->getExpiry();
+        }
+
+        if (is_string($expiry)) {
+            $expiry = $this->convertExpiryString($expiry);
+        }
+
+        apc_add($key, $data, $expiry);
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return \Orno\Cache\Adapter\Apc
+     */
+    public function delete($key)
+    {
+        apc_delete($key);
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return \Orno\Cache\Adapter\Apc
+     */
+    public function persist($key, $value)
+    {
+        $this->set($key, $value, 0);
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return \Orno\Cache\Adapter\Apc
+     */
+    public function increment($key, $offset = 1)
+    {
+        $value = (int) $this->get($key) + $offset;
+
+        $this->set($key, $value);
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return \Orno\Cache\Adapter\Apc
+     */
+    public function decrement($key, $offset = 1)
+    {
+        $value = (int) $this->get($key) - $offset;
+
+        $this->set($key, $value);
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return \Orno\Cache\Adapter\Apc
+     */
+    public function flush()
+    {
+        apc_clear_cache('user');
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return \Orno\Cache\Adapter\Apc
+     */
+    public function setConfig(array $config)
+    {
+        return $this;
+    }
+}
